f21d61eea50b35726812f70b3a5f641b203d25ca
j2317 <j2317@matsue-ct.ac.jp>
2026-06-18 06:39:24 +0000

    ToDoタスクをデータベースに保存する処理を実装しよう

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
   database: 'todo_app'
 });
+
 let todos = [];
 
 router.get('/', function (req, res, next) {
@@ -26,9 +28,13 @@ router.post('/', function (req, res, next) {
     console.log('success');
   });
   const todo = req.body.add;
-  
-  todos.push(todo);
-  res.redirect('/');
+  connection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      res.redirect('/');
+    }
+  );
 });
 
 module.exports = router;
