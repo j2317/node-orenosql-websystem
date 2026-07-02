a1cc49a33256301131cb0871dbb946e1f7e0d5c9
j2317 <j2317@matsue-ct.ac.jp>
2026-07-02 06:43:42 +0000

    Navbarでヘッダーを作成しよう

--- routes/signin.js
+++ routes/signin.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
+  const userId = req.session ? req.session.userid : undefined;
+  const isAuth = Boolean(userId);
+
   res.render('signin', {
     title: 'Sign in',
+    isAuth: isAuth,
   });
 });
 
 router.post('/', function (req, res, next) {
+  const userId = req.session ? req.session.userid : undefined;
+  const isAuth = Boolean(userId);
   const username = req.body.username;
   const password = req.body.password;
 
@@ -20,6 +26,7 @@ router.post('/', function (req, res, next) {
         res.render('signin', {
           title: 'Sign in',
           errorMessage: ['ユーザが見つかりません'],
+          isAuth: isAuth,
         });
       } else {
         req.session.userid = results[0].id;
@@ -31,6 +38,7 @@ router.post('/', function (req, res, next) {
       res.render('signin', {
         title: 'Sign in',
         errorMessage: [err.sqlMessage || 'エラーが発生しました'],
+        isAuth: isAuth,
       });
     });
 });
--- routes/signup.js
+++ routes/signup.js
@@ -3,18 +3,24 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
+  const userId = req.session ? req.session.userid : undefined;
+  const isAuth = Boolean(userId);
+
   res.render('signup', {
     title: 'Sign up',
+    isAuth: isAuth,
   });
 });
 
 router.post('/', function (req, res, next) {
+  const userId = req.session ? req.session.userid : undefined;
+  const isAuth = Boolean(userId);
   const username = req.body.username;
   const password = req.body.password;
   const repassword = req.body.repassword;
 
   if (!username || !password || !repassword) {
-    return res.render('signup', { title: 'Sign up', errorMessage: ['全ての項目を入力してください'] });
+    return res.render('signup', { title: 'Sign up', errorMessage: ['全ての項目を入力してください'], isAuth: isAuth });
   }
 
   knex('users')
@@ -22,7 +28,7 @@ router.post('/', function (req, res, next) {
     .select('*')
     .then(function (result) {
       if (result.length !== 0) {
-        return res.render('signup', { title: 'Sign up', errorMessage: ['このユーザ名は既に使われています'] });
+        return res.render('signup', { title: 'Sign up', errorMessage: ['このユーザ名は既に使われています'], isAuth: isAuth });
       } else if (password === repassword) {
         knex('users')
           .insert({ name: username, password: password })
@@ -31,15 +37,15 @@ router.post('/', function (req, res, next) {
           })
           .catch(function (err) {
             console.error(err);
-            res.render('signup', { title: 'Sign up', errorMessage: [err.sqlMessage || '登録に失敗しました'] });
+            res.render('signup', { title: 'Sign up', errorMessage: [err.sqlMessage || '登録に失敗しました'], isAuth: isAuth });
           });
       } else {
-        return res.render('signup', { title: 'Sign up', errorMessage: ['パスワードが一致しません'] });
+        return res.render('signup', { title: 'Sign up', errorMessage: ['パスワードが一致しません'], isAuth: isAuth });
       }
     })
     .catch(function (err) {
       console.error(err);
-      res.render('signup', { title: 'Sign up', errorMessage: [err.sqlMessage || 'エラーが発生しました'] });
+      res.render('signup', { title: 'Sign up', errorMessage: [err.sqlMessage || 'エラーが発生しました'], isAuth: isAuth });
     });
 });
 
